9a180a5c6dd0a74c298c0bdb433175ec408247a7
Nitin892 <78948806+Nitin892@users.noreply.github.com>
2024-11-11 17:06:33 +0530

    Update urls.py

--- social_network/urls.py
+++ social_network/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/connections/',include('connections.urls')),
     path('api/connections/',include('connections.urls'))
 ]
